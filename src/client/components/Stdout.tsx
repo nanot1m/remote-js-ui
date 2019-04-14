3f42f32cbc880de6272e07d6d5979a2a12c0c45a
hazam <hazam@yandex-team.ru>
2019-04-14 17:36:54 +0500

    `white-space: pre-wrap` in <Stdout />

--- src/client/components/Stdout.tsx
+++ src/client/components/Stdout.tsx
@@ -1,67 +1,48 @@
-import React, {
-  useCallback,
-  useLayoutEffect,
-  useRef,
-  useState,
-  useContext
-} from "react";
-import styled, { ThemeContext } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 import { themeGet } from "styled-system";
 import { BgColor, FontSize, LineHeight } from "client/themes/constants";
-import { AutoSizer, List } from "react-virtualized";
+import {
+  AutoSizer,
+  List,
+  CellMeasurer,
+  CellMeasurerCache
+} from "react-virtualized";
+
+const cache = new CellMeasurerCache({
+  defaultHeight: 18,
+  fixedWidth: true
+});
 
 interface StdoutProps {
   lines: string[];
 }
 
 export const Stdout: React.FC<StdoutProps> = ({ lines }) => {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const listRef = useRef<List>(null);
-
-  const [stickedToBottom, setStickedToBottom] = useState(true);
-
-  useLayoutEffect(() => {
-    const wrapperNode = wrapperRef.current;
-    if (!wrapperNode) {
-      return;
-    }
-    if (stickedToBottom) {
-      wrapperNode.scrollTop = wrapperNode.scrollHeight;
-    }
-  }, [lines, stickedToBottom]);
-
-  const handleScroll = useCallback((event: React.UIEvent<HTMLDivElement>) => {
-    const target = event.currentTarget;
-    if (target.scrollTop + target.clientHeight >= target.scrollHeight) {
-      setStickedToBottom(true);
-    } else {
-      setStickedToBottom(false);
-    }
-  }, []);
-
-  const theme = useContext(ThemeContext);
-
   return (
-    <StyledPreWrapper ref={wrapperRef} onScroll={handleScroll}>
+    <StyledPreWrapper>
       <StyledPre>
         <AutoSizer>
-          {size => {
-            return (
-              <List
-                containerStyle={{ overflowX: "auto" }}
-                autoContainerWidth
-                ref={listRef}
-                rowRenderer={({ index, style, key }) => (
-                  <div key={key} style={style}>
-                    {lines[index]}
-                  </div>
-                )}
-                rowCount={lines.length}
-                rowHeight={theme.lineHeights[LineHeight.S]}
-                {...size}
-              />
-            );
-          }}
+          {size => (
+            <List
+              autoContainerWidth
+              deferredMeasurementCache={cache}
+              rowRenderer={({ index, style, key, parent }) => (
+                <CellMeasurer
+                  cache={cache}
+                  columnIndex={0}
+                  key={key}
+                  parent={parent}
+                  rowIndex={index}
+                >
+                  <div style={style}>{lines[index]}</div>
+                </CellMeasurer>
+              )}
+              rowCount={lines.length}
+              rowHeight={cache.rowHeight}
+              {...size}
+            />
+          )}
         </AutoSizer>
       </StyledPre>
     </StyledPreWrapper>
@@ -80,6 +61,7 @@ export const StyledPre = styled.pre`
   font-size: ${themeGet(`fontSizes.${FontSize.S}`)}px;
   line-height: ${themeGet(`lineHeights.${LineHeight.S}`)}px;
   position: relative;
+  white-space: pre-wrap;
   height: 100%;
   margin: 0;
 `;
